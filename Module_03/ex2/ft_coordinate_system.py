7fd097dab24a491d9b05ac37c7d40117912203b8
Lexxythelizard <Lenina.vorbau@posteo.de>
2026-09-01 11:50:38 +0200

    modified Module_03/ex2/ft_coordinate_system.py: make it more OoP and more intuitive :)

--- Module_03/ex2/ft_coordinate_system.py
+++ Module_03/ex2/ft_coordinate_system.py
@@ -19,57 +19,109 @@ distance_to_center_str = "Distance to center: %.4f"
 distance_between_str = "Distance between the 2 sets of coordinates: %.4f"
 error_convert_str = "Error on parameter '%s': "
 error_convert_str += "could not convert string to float: '%s'"
+error_keyboard_interrupt_str = "\nKeyboardInterrupt: stopping execution!"
+
+dil = ','
+origin_coords = (0.0, 0.0, 0.0)
 
 # +++++++++++++++++++++++++++ classes +++++++++++++++++++++++++++
+
+
+class Pos:
+
+    _coords: tuple[float, float, float]
+
+    def __init__(
+        self,
+        coords: tuple[float, float, float]
+    ) -> None:
+        self._coords = coords
+
+    def get_coords(
+        self
+    ) -> tuple[float, float, float]:
+        return (self._coords)
+
+    def get_distance_to(
+        self,
+        coords: tuple[float, float, float]
+    ) -> float:
+
+        x0: float
+        x1: float
+        y0: float
+        y1: float
+        z0: float
+        z1: float
+
+        x0, y0, z0 = self._coords
+        x1, y1, z1 = coords
+
+        distance = math.sqrt(
+            (x0 - x1) ** 2 + (y0 - y1) ** 2 + (z0 - z1) ** 2
+        )
+        return (distance)
 
 
 # +++++++++++++++++++++++++++ funcs +++++++++++++++++++++++++++
 
 
-# --------------- process ---------------
+# --------------- cli interface ---------------
 
 
 def get_player_pos() -> tuple[float, float, float]:
 
-    while True:
-        input_str = input(input_prompt_str)
-        coords_str = input_str.split(',')
+    control: bool
+    usr_coords: tuple[float, float, float]
 
-        if len(coords_str) != 3:
+    control = False
+    usr_coords = origin_coords
+
+    while (not control):
+        control, usr_coords = get_player_pos_iteration()
+
+    return (usr_coords)
+
+
+def get_player_pos_iteration() -> tuple[bool, tuple[float, float, float]]:
+
+    usr_inp: str
+    dim: int
+    i: int
+    usr_inp_filtered: list[str]
+    usr_inp_parsed: list[float]
+    usr_coords: tuple[float, float, float]
+
+    dim = 3
+    i = 0
+
+    usr_inp = input(input_prompt_str)
+    usr_inp_filtered = usr_inp.split(dil)
+    usr_inp_filtered = [el.strip() for el in usr_inp_filtered if el.strip()]
+    usr_inp_parsed = []
+    usr_coords = origin_coords
+
+    if (usr_inp.count(dil) != 2):
+        print(invalid_syntax_str)
+        return ((False, origin_coords))
+
+    while (i < dim):
+        try:
+            usr_inp_parsed += [float(usr_inp_filtered[i])]
+        except IndexError:
             print(invalid_syntax_str)
-            continue
+            return ((False, usr_coords))
+        except ValueError:
+            print(
+                error_convert_str %
+                (usr_inp_filtered[i], usr_inp_filtered[i])
+            )
+            return ((False, usr_coords))
+        i += 1
 
-        coords: list[float] = []
-        error_occurred = False
+    usr_coords = (usr_inp_parsed[0], usr_inp_parsed[1], usr_inp_parsed[2])
 
-        for coord_str in coords_str:
-            try:
-                coords.append(float(coord_str.strip()))
-            except ValueError:
-                problematic = coord_str.strip()
-                print(error_convert_str % (problematic, problematic))
-                error_occurred = True
-                break
-
-        if not error_occurred:
-            return (coords[0], coords[1], coords[2])
-
-
-# --------------- utils ---------------
-
-
-def distance_to_origin(coords: tuple[float, float, float]) -> float:
-
-    x, y, z = coords
-    return math.sqrt(x**2 + y**2 + z**2)
-
-
-def distance_between(coords1: tuple[float, float, float],
-                     coords2: tuple[float, float, float]) -> float:
-
-    x1, y1, z1 = coords1
-    x2, y2, z2 = coords2
-    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+    return ((True, usr_coords))
 
 
 # --------------- main ---------------
@@ -77,26 +129,38 @@ def distance_between(coords1: tuple[float, float, float],
 
 def main() -> None:
 
-    first_coords: tuple[float, float, float]
-    second_coords: tuple[float, float, float]
-    distance_origin: float
-    distance_coords: float
+    pos_origin: Pos
+    pos_1: Pos
+    pos_2: Pos
 
-    print(intro_str)
+    print(intro_str, '\n')
+
+    pos_origin = Pos(origin_coords)
 
     print(first_coords_str)
-    first_coords = get_player_pos()
-    print(got_tuple_str % (first_coords,))
-    x, y, z = first_coords
-    print(coords_display_str % (x, y, z))
-    distance_origin = distance_to_origin(first_coords)
-    print(distance_to_center_str % distance_origin)
+    try:
+        pos_1 = Pos(get_player_pos())
+    except KeyboardInterrupt:
+        print(error_keyboard_interrupt_str)
+        return
+    print(coords_display_str % pos_1.get_coords())
+    print(
+        distance_to_center_str %
+        pos_1.get_distance_to(pos_origin.get_coords())
+    )
+    print('')
 
     print(second_coords_str)
-    second_coords = get_player_pos()
-    distance_coords = distance_between(first_coords, second_coords)
-    print(distance_between_str % distance_coords)
-
+    try:
+        pos_2 = Pos(get_player_pos())
+    except KeyboardInterrupt:
+        print(error_keyboard_interrupt_str)
+        return
+    print(coords_display_str % pos_2.get_coords())
+    print(
+        distance_between_str %
+        pos_2.get_distance_to(pos_1.get_coords())
+    )
 
 # +++++++++++++++++++++++++++ run +++++++++++++++++++++++++++
 
