727228b19f28f6624542273bc6aba80158912fdd
Lexxy <lenina.vorbau@posteo.de>
2026-09-17 19:22:05 +0200

    started to work on Module07
    	files:
    		- ./battle.py
    		- ./ex0		(module)
    		- ./ex0/__init__.py
    		- ./ex0/base.py			-> contains ABC
    		- ./ex0/creature_obj.py		-> will define Creature classes
    		- ./ex0/factory.py		-> will contain Factories

---
+++ Module_07/ex0/__init__.py
@@ -0,0 +1,9 @@
+from ex0.creature_obj import sniggle
+
+__version__ = "x.x.x"
+__author__ = "lenivorb"
+
+__all__ = [
+    "__version__", "__author__",
+    "sniggle"
+]
---
+++ Module_07/ex0/base.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+# ++++++++++++++++++++++++++++ imports ++++++++++++++++++++++++++++
+
+import abc
+import typing
+
+# ---------------------------- sniggle ----------------------------
+
+
+# ++++++++++++++++++++++++++++ globals ++++++++++++++++++++++++++++
+
+# ---------------------------- strings ----------------------------
+
+class StringContainer:
+
+    spaceholder = "[Spaceholder]"
+    describtion = "%s is a %s type Creature"
+    object_str = "Creature: {name: %s, type: %s}"
+    attack = "%s uses %s!"
+
+# ---------------------------- sniggle ----------------------------
+
+
+# ++++++++++++++++++++++++++++ classes ++++++++++++++++++++++++++++
+
+# ---------------------------- abstr/par ----------------------------
+
+
+class Creature(abc.ABC):
+
+    """
+    Generic Creature
+    """
+
+    __name: str = "Gen Creature"
+    __type: str = "gen Type"
+
+    def __init__(self, name: str = "", typus: str = "") -> None:
+
+        if (not isinstance(name, str)):
+            raise TypeError(StringContainer.spaceholder)
+        if (not isinstance(typus, str)):
+            raise TypeError(StringContainer.spaceholder)
+        if (name):
+            self.__name = name
+        if (typus):
+            self.__type = typus
+
+    def __str__(self) -> str:
+        return (StringContainer.object_str % (self.__name, self.__type))
+
+    def describe(self) -> str:
+        return (StringContainer.desrcibtion % (self.__name, self.__type))
+
+    @abc.abstractmethod
+    def attack(self, opponent: typing.Any = None) -> str:
+        pass
+
+
+class Factory(abc.ABC):
+
+    """
+    Generic Factory
+    """
+
+    @classmethod
+    @abc.abstractmethod
+    def create_base(cls) -> Creature:
+        pass
+
+    @classmethod
+    @abc.abstractmethod
+    def create_evolved(cls) -> Creature:
+        pass
